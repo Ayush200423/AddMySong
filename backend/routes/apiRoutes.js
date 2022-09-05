ee7d50ac22d9d8b6fc26d024f1873f3a19c0621b
Ayush Patel <ayush200423@gmail.com>
2022-09-05 10:53:54 -0700

    setup backend api endpoints to create and read

--- backend/routes/apiRoutes.js
+++ backend/routes/apiRoutes.js
@@ -1,22 +1,34 @@
 const express = require('express')
 const router = express.Router()
+var ObjectID = require('mongodb').ObjectID;
 const Code = require('../models/codesModel')
 
-// get list of id's
-router.get('/:id', (req, res) => {
-    console.log(req.params.id)
-    Code.find({}, (err, results) => {
-        if (err) {
-            res.json(err)
-        } else {
-            res.json(results)
-        }
-    })
+// get array of hosted rooms' playlist ids with specified field id
+router.get('/rooms', (req, res) => {
+    try {
+        let fieldId = ObjectID(req.headers.id)
+        Code.findById(fieldId, (err, results) => {
+            if (err) {
+                res.json(err)
+            } else {
+                res.json(results)
+            }
+        })
+    } catch {
+        res.json('An error occurred - Invalid ID')
+    }
 })
 
-// create new id
-router.post('/:id', (req, res) => {
-    res.status(200).json({message: `get ${req.params.id}`})
+// create new room with playlist uri
+router.post('/rooms', (req, res) => {
+    let data = new Code({"playlistCode": req.params.id})
+    data.save()
+    .then((item) => {
+        res.send(item)
+    })
+    .catch(() => {
+        res.status(200).send('Error - Something went wrong')
+    })
 })
 
 module.exports = router
